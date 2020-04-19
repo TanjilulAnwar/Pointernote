ca25fe4e9b8ae4440105137d9ba89ccc4fbc4beb
Tanjilul Anwar <36304879+TanjilulAnwar@users.noreply.github.com>
2020-04-19 09:19:12 +0600

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, i, *ptr, sum = 0;
+    
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    ptr = (int*) malloc(n * sizeof(int));
+ 
+    // if memory cannot be allocated
+    if(ptr == NULL)                     
+    {
+        printf("Error! memory not allocated.");
+        exit(0);
+    }
+
+    printf("Enter elements: ");
+    for(i = 0; i < n; ++i)
+    {
+        scanf("%d", ptr + i);
+         printf("added = %d Adress= %p \n", *(ptr + i),ptr+i);
+         
+        sum += *(ptr + i);
+    }
+
+    printf("Sum = %d\n ", sum);
+  
+    // deallocating the memory
+    free(ptr);
+printf("ptr = %p ", ptr);
+printf("val = %d \n", *ptr);
+
+printf("ptr = %p ", ptr+1);
+printf("val = %d ", *(ptr+1));
+    return 0;
+}
